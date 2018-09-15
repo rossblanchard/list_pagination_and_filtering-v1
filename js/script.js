b2ae7f42642d3b97d79a2c8cc072dc688d8294e6
Ross Blanchard <rosslblanchard@gmail.com>
2018-09-14 21:27:13 -0700

    minumum requirements

--- js/script.js
+++ js/script.js
@@ -5,24 +5,55 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 
+const studentList = document.getElementsByClassName('student-item cf');
+const listLength = studentList.length;
+let page = 1 ;
+const itemsPerPage = 10;
+const totalPages = Math.ceil(listLength/itemsPerPage) ;
+const pageDiv = document.querySelector('.page');
 
+const showPageItems = (page,items) => {
+    let start = (page * 10) -10;
+    let stop = (start + 9);
+    for (let x=0; x < items.length; x++ ) {
+      if (x >= start && x <= stop)  {
+          items[x].style.display = 'block';
+      } else {
+          items[x].style.display = 'none';
+      }
+    }
+}
+showPageItems(page,studentList);
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+//got hints from https://github.com/awillem/project-2-list-pagination/blob/master/js/script.js
 
+ const pagination = (pageCount) => {
+    const paginationDiv = document.createElement('div');
+    paginationDiv.className = 'pagination';
+    const paginationUl = document.createElement('ul');
 
+    for (y = 1; y <= pageCount; y++) {
+      const paginationLi = document.createElement('li');
+      const paginationA = document.createElement('a');
+      if (y === page){
+          paginationA.className = "active";
+      }
+      paginationA.href = "#";
+      paginationA.textContent = y;
+      paginationLi.appendChild(paginationA);
+      paginationUl.appendChild(paginationLi);
+      // listener
+      paginationA.addEventListener("click", (e) => {
+        let prevActive = document.querySelector('.active');
+        prevActive.classList.remove('active');
+        page = paginationA.textContent;
+        e.target.className = "active";
+        showPageItems(page,studentList);
+    });
 
+    paginationDiv.appendChild(paginationUl);
+    pageDiv.appendChild(paginationDiv);
+    }
+}
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
-
-
-
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
+pagination(totalPages);
